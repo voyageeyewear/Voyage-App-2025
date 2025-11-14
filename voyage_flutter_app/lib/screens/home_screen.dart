b22b023f5f037027fa663ce58264ccace4931a80
Dhruv <dhruv@Dhruvs-MacBook-Air.local>
2025-11-14 15:54:55 +0530

    Revert menu spacing to previous layout

--- voyage_flutter_app/lib/screens/home_screen.dart
+++ voyage_flutter_app/lib/screens/home_screen.dart
@@ -83,20 +83,14 @@ class _HomeScreenState extends State<HomeScreen> {
               
               // App Header below announcement
               AppBar(
-                leading: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.menu, size: 24, color: Colors.black),
-                      onPressed: () {
-                        NavigationDrawerWidget.show(context);
-                      },
-                      padding: EdgeInsets.zero,
-                    ),
-                    const SizedBox(width: 72),
-                  ],
+                leading: IconButton(
+                  icon: const Icon(Icons.menu, size: 24, color: Colors.black),
+                  onPressed: () {
+                    NavigationDrawerWidget.show(context);
+                  },
+                  padding: EdgeInsets.zero,
                 ),
-                leadingWidth: 120,
+                leadingWidth: 56,
                 title: Image.network(
                   'https://www.voyageeyewear.com/cdn/shop/files/Voyage_hindi_2.png?v=1720597430&width=350',
                   height: 26,
